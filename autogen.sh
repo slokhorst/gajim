d7f4ae2619349b91c5dd96e855f3a3400b73005d
js <js-gajim@webkeks.org>
2008-08-24 08:15:56 +0000

    configure works on OS X without problems now.
    * Removed all automake files.
    * Removed automake stuff from configure.ac.
    * Removed automake stuff from autogen.sh.
    * Removed libtool stuff.
    * Added the files of the new buildsys.

--- autogen.sh
+++ autogen.sh
@@ -1,28 +1,17 @@
-#!/usr/bin/env bash
-  AM_ARGS="--add-missing --gnu --copy"
-  CONF_ARGS=""
-  if test x`uname -s 2>/dev/null` = 'xDarwin' -a -f /Library/Frameworks/GTK+.framework/Versions/Current/env; then
-    . /Library/Frameworks/GTK+.framework/Versions/Current/env
-    AM_ARGS="${AM_ARGS} --ignore-deps"
-    CONF_ARGS="${CONF_ARGS} --disable-idle --without-x"
-  fi
-
-  echo "[encoding: UTF-8]" > po/POTFILES.in \
-  && ls -1 data/gajim.desktop.in.in data/glade/*.glade \
-  src/*py src/common/*py src/common/zeroconf/*.py src/osx/*.py >> \
-  po/POTFILES.in || exit 1
-  if test -z `which pkg-config 2>/dev/null`;then
-    echo "***Error: pkg-config not found***"
+#!/bin/sh
+if ! which pkg-config >/dev/null 2>&1; then
+	echo "*** ERROR: pkg-config not found ***"
 	echo "See README.html for build requirements."
 	exit 1
-  fi
+fi
 
-  which glibtoolize >/dev/null 2>&1 && LIBTOOLIZE="glibtoolize" || LIBTOOLIZE="libtoolize"
+echo "[encoding: UTF-8]" > po/POTFILES.in && \
+ls -1 data/gajim.desktop.in.in data/glade/*.glade \
+	src/*.py src/common/*.py src/common/zeroconf/*.py src/osx/*.py \
+	>>po/POTFILES.in || exit 1
 
-  intltoolize --force --automake \
-  && aclocal -I ./m4 \
-  && $LIBTOOLIZE --copy --force --automake \
-  && autoheader \
-  && autoconf  \
-  && automake ${AM_ARGS} \
-  && ./configure ${CONF_ARGS} $@
+intltoolize --force && \
+aclocal -I ./m4 && \
+autoheader && \
+autoconf && \
+./configure ${CONF_ARGS} $@
